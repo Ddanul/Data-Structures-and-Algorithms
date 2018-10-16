1c3067a397bfc6d433ee10906cdbb002de76e68a
Danul De Leon <danul.deleon@gmail.com>
2018-10-16 11:06:38 -0700

    moved directory

---
+++ DataStructures/LinkedLists/LinkedLists/Program.cs
@@ -0,0 +1,24 @@
+﻿using LinkedLists.Classes;
+using System;
+
+namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Node node1 = new Node(7);
+            Node node2 = new Node("cat");
+            Node node3 = new Node(12);
+
+            LList myAwesomeList = new LList(node1);
+            myAwesomeList.Append(node2);
+            myAwesomeList.AddAfter(node3, node2);
+
+            myAwesomeList.Print();
+
+        }
+    }
+}
