80b314f19c64a682fcd0aa8c9b31be35104af138
Danul De Leon <danul.deleon@gmail.com>
2018-10-16 09:16:00 -0700

    set up files

---
+++ Challenges/LinkedLists/LinkedLists/Program.cs
@@ -0,0 +1,22 @@
+﻿using LinkedLists.Classes;
+using System;
+
+namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Node node1 = new Node(7);
+            Node node2 = new Node("cat");
+
+            LList myAwesomeList = new LList(node1);
+            myAwesomeList.Append(node2);
+
+            myAwesomeList.Print();
+
+        }
+    }
+}
