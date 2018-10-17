af263e15606087850a97bc7a555d4ff2246817d3
Danul De Leon <danul.deleon@gmail.com>
2018-10-17 09:53:10 -0700

    all tests passing

---
+++ Challenges/SinglyLinkedLists/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+using LinkedLists.Classes;
+using SinglyLinkedLists;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(4, "Manolo")]
+        [InlineData(3, 172)]
+        [InlineData(0, "Donut")]
+        [InlineData(-1, "Invalid k value")]
+        public void LLKthFromTheEndReturnsProperValue(int value, object expectation)
+        {
+            //Arrange
+            Node node1 = new Node("Manolo");
+            Node node2 = new Node(172);
+            Node node3 = new Node("Brooke");
+            Node node4 = new Node(98);
+            Node node5 = new Node("Donut");
+
+            LList llist = new LList(node1);
+            llist.Append(node2);
+            llist.Append(node3);
+            llist.Append(node4);
+            llist.Append(node5);
+
+            //Assert
+            Assert.Equal(expectation, Program.LLKthFromEnd(value, llist));
+        }
+    }
+}
