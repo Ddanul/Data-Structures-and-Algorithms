c13ed8f011cf75b7cfdda2fe445371f3670c4b61
Danul De Leon <danul.deleon@gmail.com>
2018-10-18 11:06:03 -0700

    all tests passing

--- Challenges/MergeList/XUnitTestProject1/UnitTest1.cs
+++ Challenges/MergeList/XUnitTestProject1/UnitTest1.cs
@@ -25,20 +25,62 @@ namespace XUnitTestProject1
             LList list2 = new LList(node2);
             list2.Append(node4);
 
-            Node node1t = new Node(1);
-            Node node2t = new Node(2);
-            Node node3t = new Node(3);
-            Node node4t = new Node(4);
-
-            LList testList = new LList(node1t);
-            testList.Append(node2t);
-            testList.Append(node3t);
-            testList.Append(node4t);
-
             LList mergeList = Program.Merge(list1, list2);
 
             //Assert
             Assert.Equal("1 --> 2 --> 3 --> 4 --> NULL", Program.Print(mergeList));
+        }
+
+        /// <summary>
+        /// Test to merge unequal length lists, first list longer
+        /// </summary>
+        [Fact]
+        public void MatchMergedListOfFirstListLonger()
+        {
+            //Arrange
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+            Node node4 = new Node(4);
+            Node node5 = new Node(5);
+
+            LList list1 = new LList(node1);
+            list1.Append(node3);
+            list1.Append(node5);
+
+            LList list2 = new LList(node2);
+            list2.Append(node4);
+
+            LList mergeList = Program.Merge(list1, list2);
+
+            //Assert
+            Assert.Equal("1 --> 2 --> 3 --> 4 --> 5 --> NULL", Program.Print(mergeList));
+        }
+
+        /// <summary>
+        /// Test to merge unequal length lists, second list longer
+        /// </summary>
+        [Fact]
+        public void MatchMergedListOfSecondListLonger()
+        {
+            //Arrange
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+            Node node4 = new Node(4);
+            Node node5 = new Node(5);
+
+            LList list1 = new LList(node1);
+            list1.Append(node3);
+
+            LList list2 = new LList(node2);
+            list2.Append(node4);
+            list2.Append(node5);
+
+            LList mergeList = Program.Merge(list1, list2);
+
+            //Assert
+            Assert.Equal("1 --> 2 --> 3 --> 4 --> 5 --> NULL", Program.Print(mergeList));
         }
     }
 }
