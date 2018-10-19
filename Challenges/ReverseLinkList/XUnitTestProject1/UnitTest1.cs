3738733306a5194c93fff3c142dd57885e0414ff
Danul De Leon <danul.deleon@gmail.com>
2018-10-18 19:35:37 -0700

    added test passed

--- Challenges/ReverseLinkList/XUnitTestProject1/UnitTest1.cs
+++ Challenges/ReverseLinkList/XUnitTestProject1/UnitTest1.cs
@@ -27,8 +27,29 @@ namespace XUnitTestProject1
 
             //Assert
             Assert.Equal("4 --> 3 --> 2 --> 1 --> NULL", Program.Print(Program.Reverse(list1)));
+            
+        }
 
+        /// <summary>
+        /// Test to make sure method works with texts
+        /// </summary>
+        [Fact]
+        public void ReturnsReversedStringVals()
+        {
+            //Arrange
 
+            Node node1 = new Node("p");
+            Node node2 = new Node("a");
+            Node node3 = new Node("r");
+            Node node4 = new Node("k");
+
+            LList list1 = new LList(node1);
+            list1.Append(node2);
+            list1.Append(node3);
+            list1.Append(node4);
+
+            //Assert
+            Assert.Equal("k --> r --> a --> p --> NULL", Program.Print(Program.Reverse(list1)));
 
         }
     }
