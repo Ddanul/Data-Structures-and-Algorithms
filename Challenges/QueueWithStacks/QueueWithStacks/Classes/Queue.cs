768e31d4f7ace35ec41c75a5faf215a05a9988e6
Danul De Leon <danul.deleon@gmail.com>
2018-10-22 20:45:42 -0700

    methods are working

---
+++ Challenges/QueueWithStacks/QueueWithStacks/Classes/Queue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueWithStacks.Classes
+{
+    class Queue
+    {
+        public Stack s1 = new Stack();
+        public Stack s2 = new Stack();
+
+        public Queue(Node node)
+        {
+            s1.Push(node);
+        }
+
+        /// <summary>
+        /// Adds new node to the end of the queue
+        /// </summary>
+        /// <param name="node">new node</param>
+        public void Enqueue(Node node)
+        {
+            s1.Push(node);
+        }
+
+        /// <summary>
+        /// Removes node from the front of the queue
+        /// </summary>
+        /// <returns>removed node</returns>
+        public Node Dequeue()
+        {
+            //transer all nodes from s1 to s2 for correct order
+            while(s1.Peek() != null)
+            {
+                s2.Push(s1.Pop());
+            }
+            //Node that is being dequeued, "front".
+            Node temp = s2.Pop();
+
+            //transfer all nodes back to s1 for correct order with enqueue
+            while(s2.Peek() != null)
+            {
+                s1.Push(s2.Pop());
+            }
+
+            return temp;
+        }
+    }
+}
+
--- Challenges/QueueWithStacks/QueueWithStacks/Program.cs
+++ Challenges/QueueWithStacks/QueueWithStacks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using QueueWithStacks.Classes;
 
 namespace QueueWithStacks
 {
@@ -7,6 +8,23 @@ namespace QueueWithStacks
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            QueueWithStacks();
+        }
+
+        static void QueueWithStacks()
+        {
+            Node n1 = new Node(1);
+            Node n2 = new Node(2);
+            Node n3 = new Node(3);
+            Node n4 = new Node(4);
+
+            Queue myQ = new Queue(n1);
+            myQ.Enqueue(n2);
+            myQ.Enqueue(n3);
+            Console.WriteLine($"The last node added was: {myQ.s1.Top.Value}");
+
+            Console.WriteLine($"Dequeued node: {myQ.Dequeue().Value}");
+
         }
     }
 }
