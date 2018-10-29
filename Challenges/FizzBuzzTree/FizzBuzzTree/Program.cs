291210b1e47f19b384c0203b306f8fc467cfe649
Danul De Leon <danul.deleon@gmail.com>
2018-10-29 13:56:46 -0700

    algo is working

--- Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -14,12 +14,46 @@ namespace FizzBuzzTree
             Node n3 = new Node(15);
             Node n4 = new Node(2);
 
-            BinarySearchTree 
+            BinaryTree bst = new BinaryTree(n1);
+            bst.Root.Left = n2;
+            bst.Root.Right = n3;
+            bst.Root.Left.Left = n4;
+
+            FizzBuzzTree(bst.Root);
         }
 
-        static List<int> FizzBuzzTree()
+        static void FizzBuzzTree(Node node)
         {
+            if(node == null)
+            {
+                Console.WriteLine("Ain't nothin here");
+            }
+            if(node.Left != null)
+            {
+                FizzBuzzTree(node.Left);
+            }
 
+            if(node.Value % 15 == 0)
+            {
+                Console.Write(" FizzBuzz ");
+            }
+            else if(node.Value % 3 == 0)
+            {
+                Console.Write(" Fizz ");
+            }
+            else if(node.Value % 5 == 0)
+            {
+                Console.Write(" Buzz ");
+            }
+            else
+            {
+                Console.Write($"{node.Value}");
+            }
+
+            if (node.Right != null)
+            {
+                FizzBuzzTree(node.Right);
+            }
         }
     }
 }
