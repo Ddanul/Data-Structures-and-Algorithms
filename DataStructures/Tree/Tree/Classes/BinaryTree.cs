72dc0a389c621833b9ca97d294aae2f22d0525b8
Danul De Leon <danul.deleon@gmail.com>
2018-10-29 10:27:17 -0700

    preorder working

---
+++ DataStructures/Tree/Tree/Classes/BinaryTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    class BinaryTree
+    {
+        public Node Root { get; set; }
+        List<int> searchValues = new List<int>();
+
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+        public List<int> PreOrder(Node node)
+        {
+            int value = node.Value;
+            searchValues.Add(value);
+
+            if(node.Left != null)
+            {
+                PreOrder(node.Left);
+            }
+            if(node.Right != null)
+            {
+                PreOrder(node.Right);
+            }
+
+            return searchValues;
+        }
+    }
+}
---
+++ DataStructures/Tree/Tree/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree.Classes;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Node n1 = new Node(1);
+            Node n2 = new Node(2);
+            Node n3 = new Node(3);
+            Node n4 = new Node(4);
+            Node n5 = new Node(5);
+
+            BinaryTree tree1 = new BinaryTree(n1);
+            tree1.Root.Left = n2;
+            tree1.Root.Right = n3;
+            tree1.Root.Left.Left = n4;
+            tree1.Root.Left.Right = n5;
+
+            Print(tree1.PreOrder(tree1.Root));
+        }
+
+        static void Print(List<int> values)
+        {
+            foreach(int value in values)
+            {
+                Console.Write($"{value}");
+            }
+        }
+    }
+}
