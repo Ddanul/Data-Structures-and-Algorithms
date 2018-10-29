4586924d6718c07f7ae12124251082bf27774568
Danul De Leon <danul.deleon@gmail.com>
2018-10-29 13:13:46 -0700

    initial commit

---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree.Classes;
+
+namespace FizzBuzzTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Node n1 = new Node(3);
+            Node n2 = new Node(5);
+            Node n3 = new Node(15);
+            Node n4 = new Node(2);
+
+            BinarySearchTree 
+        }
+
+        static List<int> FizzBuzzTree()
+        {
+
+        }
+    }
+}
