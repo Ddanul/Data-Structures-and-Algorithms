fa47efed3723c7545bec93fa043479a456846e7a
Danul De Leon <danul.deleon@gmail.com>
2018-10-31 21:56:46 -0700

    traversal printing out correct numbers

---
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Classes/BinaryTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadthFirstTraversal.Classes
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+        public List<object> searchValues = new List<object>();
+
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+        /// <summary>
+        /// Pre Order Traversal root, left, right
+        /// </summary>
+        /// <param name="node">root node</param>
+        /// <returns>list of integers in order</returns>
+        public List<object> PreOrder(Node node)
+        {
+            object value = node.Value;
+            searchValues.Add(value);
+
+            if (node.Left != null)
+            {
+                PreOrder(node.Left);
+            }
+            if (node.Right != null)
+            {
+                PreOrder(node.Right);
+            }
+
+            return searchValues;
+        }
+
+        /// <summary>
+        /// In order traversal of tree
+        /// </summary>
+        /// <param name="node">root node</param>
+        /// <returns>list of integers in order</returns>
+        public List<object> InOrder(Node node)
+        {
+            if (node.Left != null)
+            {
+                InOrder(node.Left);
+            }
+
+            searchValues.Add(node.Value);
+
+            if (node.Right != null)
+            {
+                InOrder(node.Right);
+            }
+
+            return searchValues;
+        }
+
+        /// <summary>
+        /// Post order traversal of tree
+        /// </summary>
+        /// <param name="node">root node</param>
+        /// <returns>list of integers in order</returns>
+        public List<object> PostOrder(Node node)
+        {
+            if (node.Left != null)
+            {
+                PostOrder(node.Left);
+            }
+
+            if (node.Right != null)
+            {
+                PostOrder(node.Right);
+            }
+
+            searchValues.Add(node.Value);
+
+            return searchValues;
+        }
+    }
+}
---
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Program.cs
@@ -0,0 +1,50 @@
+﻿using BreadthFirstTraversal.Classes;
+using System;
+
+namespace BreadthFirstTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Node n1 = new Node(1);
+            Node n2 = new Node(2);
+            Node n3 = new Node(3);
+            Node n4 = new Node(4);
+            Node n5 = new Node(5);
+            Node n6 = new Node(6);
+            Node n7 = new Node(7);
+
+            BinaryTree bst = new BinaryTree(n1);
+            bst.Root.Left = n2;
+            bst.Root.Right = n3;
+            bst.Root.Left.Left = n4;
+            bst.Root.Left.Right = n5;
+            bst.Root.Right.Left = n6;
+            bst.Root.Right.Right = n7;
+
+            BreadthFirst(bst.Root);
+            Console.ReadLine();
+        }
+
+        public static void BreadthFirst(Node root)
+        {
+            Queue q = new Queue(root);
+            while(q.Peek() != null)
+            {
+                var Front = q.Dequeue();
+                Console.WriteLine(Front.Value);
+
+                if(Front.Left != null)
+                {
+                    q.Enqueue(Front.Left);
+                }
+                if(Front.Right != null)
+                {
+                    q.Enqueue(Front.Right);
+                }
+            }
+        }
+    }
+}
