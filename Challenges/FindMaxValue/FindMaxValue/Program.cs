c1ad6b78b904c53baec54dc03af65bd2f66539d1
Danul De Leon <danul.deleon@gmail.com>
2018-11-04 19:27:31 -0800

    find max value

---
+++ Challenges/FindMaxValue/FindMaxValue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree.Classes;
+
+namespace FindMaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Node n1 = new Node(1);
+            Node n2 = new Node(570);
+            Node n3 = new Node(5);
+            BinaryTree tree = new BinaryTree(n1);
+            tree.Root.Left = n2;
+            tree.Root.Right = n3;
+
+            Console.WriteLine(FindMaxValue(tree));
+        }
+
+        static int FindMaxValue(BinaryTree tree)
+        {
+            List<int> listOfNodes = tree.PreOrder(tree.Root);
+            int temp = tree.Root.Value;
+            foreach(var value in listOfNodes)
+            {
+                if (temp < value)
+                {
+                    temp = value;
+                }
+            }
+            return temp;
+        }
+
+    }
+}
