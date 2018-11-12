9349cf8cc5d9ea5994203d0867c46f468a408c5b
Danul De Leon <danul.deleon@gmail.com>
2018-11-12 11:03:40 -0800

    initial commit

---
+++ DataStructures/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtables.Classes
+{
+    public class Hashtable
+    {
+        public object[] hashtable = new object[100];
+
+        /// <summary>
+        /// returns integer used for index
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>index integer</returns>
+        public int GetHash(string key)
+        {
+            byte[] ASCIIValues = Encoding.ASCII.GetBytes(key);
+            int val = 0;
+            foreach (byte b in ASCIIValues)
+            {
+                val += b;
+            }
+
+            val = val * 499;
+            val = val % hashtable.Length;
+            return val;
+        }
+
+        public void Add(Node newNode)
+        {
+            int index = GetHash(newNode.Key);
+            hashtable[index].Append(newNode);
+        }
+    }
+
+}
