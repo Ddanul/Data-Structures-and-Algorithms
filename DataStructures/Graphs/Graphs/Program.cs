71733acb162b79857ff891789da664a7b877bae1
Danul De Leon <danul.deleon@gmail.com>
2018-11-27 23:23:57 -0800

    added readme

---
+++ DataStructures/Graphs/Graphs/Program.cs
@@ -0,0 +1,35 @@
+﻿using Graphs.Classes;
+using System;
+
+namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Graph graph = new Graph();
+
+            Node node = new Node { Value = "chicken" };
+            Tuple<Node, int> child = new Tuple<Node, int>(node, 2);
+
+            graph.AddEdge("flamingo", child);
+
+            Console.WriteLine("All Nodes:");
+            var allNodes = graph.GetNodes();
+
+            foreach(var item in allNodes)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+            Console.WriteLine("Neighbor Nodes for Cat:");
+            var neighbors = graph.GetNeighbors("cat");
+
+            foreach (var item in neighbors)
+            {
+                Console.WriteLine(item.Item1.Value);
+            }
+        }
+    }
+}
